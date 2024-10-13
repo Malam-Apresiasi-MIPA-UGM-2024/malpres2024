36e6fc8e3078f2656ac710a302e3fdafb09d2960
HappyRehund <rayhanardian045@gmail.com>
2024-10-13 10:56:16 +0700

    nominasi tinggal ornament di hero #2

--- app/_components/nominasicard.jsx
+++ app/_components/nominasicard.jsx
@@ -6,7 +6,7 @@ const Card = ({
             <div className="border bg-[#D9D9D9] min-h-44 h-full w-full md:min-h-40 md:min-w-44 md:max-w-48 lg:min-w-48">
                 <img src="" alt="" />
             </div>
-            <div className="min-h-52 w-auto py-2 px-4 md:flex flex-grow lg:min ">
+            <div className="min-h-60 w-auto py-2 px-4 flex flex-col sm:justify-evenly flex-grow md:flex md:flex-row lg:min-h-auto ">
                 <div className="flex flex-col w-auto mb-auto lg:mt-4 md:pr-10">
                     <div>
                         <h2 className=" text-xl font-bold text-[#E9557F] lg:text-2xl">{title}</h2>
@@ -15,7 +15,7 @@ const Card = ({
                         <p className="text-xs md:text-sm lg:text-base mt-0 text-black mb-2 ">{desc}</p>
                     </div>
                 </div>
-                <div className="flex justify-center items-center md:mb-8 lg:mb-12 md:ml-auto ">
+                <div className="flex justify-center items-center mb-2 md:mb-8 lg:mb-12 ">
                     <button className="w-48 h-11 md:w-44 md:text-lg border bg-[#E9557F] text-white rounded-md text-sm font-semibold py-1.5">Beri Pilihanmu</button>
                 </div>
             </div>
