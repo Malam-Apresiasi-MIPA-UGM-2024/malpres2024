369ad5fd7a6b434944aff01b1a18de73c20a358e
Rama Andhika Pratama <137267391+kiyuzo@users.noreply.github.com>
2024-10-14 19:29:20 +0700

    added output and reactstrictmode

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/malpres2024',
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
