a6e9e12533424a11eb0d15ab0757bd3eebc18ccd
KiyuZo <ramaandikapratama4@gmail.com>
2024-10-15 15:32:13 +0700

    fixed npm run dev displaying 404

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/malpres2024',
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
 };
 
 export default nextConfig;
