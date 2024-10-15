041ab76d41fea9d6e91e1c1b2a4dfee136a5fe77
HappyRehund <rayhanardian045@gmail.com>
2024-10-15 15:57:13 +0700

    animation di nominasi page

--- app/_components/nominasicard.jsx
+++ app/_components/nominasicard.jsx
@@ -1,8 +1,40 @@
-const Card = ({
-    title, desc
-}) => {
+"use client"
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const Card = ({ title, desc }) => {
+    const cardRef = useRef(null);
+
+    useEffect(() => {
+        // Apply GSAP animation on the Card when it comes into view
+        gsap.fromTo(
+            cardRef.current,
+            {
+                opacity: 0,
+                scale: 0.5,
+            },
+            {
+                opacity: 1,
+                scale: 1,
+                duration: 1,
+                ease: "power2.out",
+                scrollTrigger: {
+                    trigger: cardRef.current,
+                    start: "top 80%", // Starts animation when top of the card is 80% from the top of the viewport
+                    toggleActions: "play none none reset", // Play the animation on scroll
+                },
+            }
+        );
+    }, []);
+
     return (
-        <div className="bg-white shadow-md w-56 flex flex-col justify-center items-start md:flex-row md:justify-start md:w-full md:h-40">
+        <div
+            ref={cardRef}
+            className="bg-white shadow-md w-56 flex flex-col justify-center items-start md:flex-row md:justify-start md:w-full md:h-40"
+        >
             <div className="border bg-[#D9D9D9] min-h-44 h-full w-full md:min-h-40 md:min-w-44 md:max-w-48 lg:min-w-48">
                 <img src="" alt="" />
             </div>
@@ -16,11 +48,13 @@ const Card = ({
                     </div>
                 </div>
                 <div className="flex justify-center items-center mb-2 md:mb-8 lg:mb-12 ">
-                    <button className="w-48 h-11 md:w-44 md:text-lg border bg-[#E9557F] text-white rounded-md text-sm font-semibold py-1.5">Beri Pilihanmu</button>
+                    <button className="w-48 h-11 md:w-44 md:text-lg border bg-[#E9557F] text-white rounded-md text-sm font-semibold py-1.5">
+                        Beri Pilihanmu
+                    </button>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Card;
