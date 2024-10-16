a7dd6475239a385ab660d25dc38407de5a471f6f
KiyuZo <ramaandikapratama4@gmail.com>
2024-10-16 11:42:17 +0700

    modified the kilas balik embed youtube video to be responsive

--- app/_components/kilasbalik.jsx
+++ app/_components/kilasbalik.jsx
@@ -14,7 +14,7 @@ const KilasBalik = () => {
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
-          className="rounded-lg"
+          className="rounded-lg w-full h-64 md:w-96 md:h-56 lg:w-[560px] lg:h-[315px] mx-[1rem]"
         ></iframe>
       </div>
     </div>
