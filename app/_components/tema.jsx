5a73d82d35463cc9a576a0e6c8bae0187a49c015
KiyuZo <ramaandikapratama4@gmail.com>
2024-10-16 11:37:15 +0700

    modified the apresiasi nominasi and tema to be responsive

--- app/_components/tema.jsx
+++ app/_components/tema.jsx
@@ -4,20 +4,17 @@ const Tema = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-[url('/bg-tema.svg')] bg-cover bg-center w-full h-full">
       <div className="w-[14rem] bg-[#FFFFFF] p-3 rounded-lg mt-[8rem]">
-        <h2 className="text-xl font-bold text-center text-[#E9557F]">Tema MALPRES</h2>
+        <h2 className="text-xl font-bold text-center text-[#E9557F]">Tentang MALPRES</h2>
       </div>
       <div className="mt-4">
-        <h1
-          className="text-[4rem] font-bold text-center"
-          style={{
-            WebkitTextStroke: '2px #DA1C5C',
-          }}
-        >
-          ADARA GANA SIDDHI
-        </h1>
+      <h1
+        className="text-5xl md:text-6xl lg:text-5xl xl:text-6xl font-bold text-center"
+      >
+        ADARA GANA SIDDHI
+      </h1>
       </div>
       <div>
-        <h2 className="text-[1.5rem] font-bold text-center text-[#EEC4A9]"
+        <h2 className="text-xl md:text-2xl lg:text-xl xl:text-2xl font-bold text-center text-[#EEC4A9]"
         style={{
           WebkitTextStroke: '1px #000000',
         }}>
