c3648b7ba2ed67c32f8ea8b8036680751d68f759
KiyuZo <ramaandikapratama4@gmail.com>
2024-10-16 23:44:18 +0700

    benerin buanyak pokoke

--- app/_components/timeline.jsx
+++ app/_components/timeline.jsx
@@ -15,15 +15,12 @@ const Timeline = () => {
         <div className="bg-white rounded-lg p-2 mb-8 w-48 text-center font-bold text-[#E9557F] text-xl relative z-10 border-2 border-black">
             Timeline
         </div>
-        <div className="flex flex-col md:flex-row justify-around w-full max-w-4xl space-y-4 md:space-y-0 md:space-x-2 relative z-10 mb-[2rem]">
-          <div className="text-center">
-            <img src="/img-timeline.svg" alt="Guest 1" className="w-full rounded-lg" />
+        <div className="flex flex-col md:flex-row justify-around w-full max-w-4xl space-y-4 md:space-y-0 md:space-x-2 relative z-10 mb-[4rem]">
+          <div className="text-center ">
+            <img src="/item-ticketing.svg" alt="Guest 1" className="w-full rounded-lg" />
           </div>
           <div className="text-center">
-            <img src="/img-timeline.svg" alt="Guest 2" className="w-full rounded-lg" />
-          </div>
-          <div className="text-center">
-            <img src="/img-timeline.svg" alt="Guest 3" className="w-full rounded-lg" />
+            <img src="/item-voting.svg" alt="Guest 2" className="w-full rounded-lg" />
           </div>
         </div>
 
