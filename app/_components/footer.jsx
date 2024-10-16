a03a76092bf121a1967613840abeb1e3d9d88fa5
KiyuZo <ramaandikapratama4@gmail.com>
2024-10-16 11:45:16 +0700

    modified the footer to be responsive

--- app/_components/footer.jsx
+++ app/_components/footer.jsx
@@ -2,13 +2,17 @@
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-between p-[2rem] bg-[url('/bg-footer.svg')] bg-no-repeat bg-cover bg-center w-full">
-      <div className="flex-1 flex items-center justify-center">
+    // CONTAINER
+    <div className="flex flex-col md:flex-row items-center justify-between p-[2rem] bg-[url('/bg-footer.svg')] bg-no-repeat bg-cover bg-center w-full">
+      {/* BOX 1 */}
+      <div className="flex-1 flex items-center justify-center mb-4 md:mb-0">
         <img src="/malpres-footer.svg" alt="MALPRES" />
       </div>
-      <div className="border-2 border-[#F9F9E0] h-[24rem] mx-4"></div>
+      {/* BOX 2 */}
+      <div className="border-2 border-[#F9F9E0] h-0 md:h-[24rem] w-full md:w-auto mx-4"></div>
+      {/* BOX 3 */}
       <div className="flex-1">
-        <ul className="list-none text-[#F9F9E0] ml-[4rem]">
+        <ul className="list-none text-[#F9F9E0] ml-0 md:ml-[4rem]">
           <li className="my-4">Jalan Puspanjolo dalam 5 no. 10, RT 011, RW 001, Semarang, Jawa Tengah, 5014</li>
           <li className="my-4">Kontak Email</li>
           <li className="my-4">Nomor Telepon</li>
