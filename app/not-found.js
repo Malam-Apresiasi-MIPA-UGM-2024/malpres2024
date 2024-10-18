7700c49be13b35b80e8d5caa1ea5afbd1cd14328
WibiLaksono <wibilaksonowijaya@mail.ugm.ac.id>
2024-10-18 11:33:46 +0700

    benerin 404

--- app/not-found.js
+++ app/not-found.js
@@ -3,7 +3,7 @@ export default function NotFound(){
     return(
         <div className="flex inset-0 justify-center items-center h-screen ">
             <div
-            className="absolute inset-0 w-full h-full"
+            className="absolute inset-0 w-fulln h-full"
             style={{
             backgroundImage: "url('/Sunburst.png')",
             backgroundSize: "cover",
@@ -14,7 +14,7 @@ export default function NotFound(){
             </div>
             <div className="flex flex-col items-center justify-center h-[20rem] w-[50rem]">
                 <h1 className="font-extrabold font-poppins text-9xl text-white text-stroke">404</h1>
-                <p className="font-extrabold text-xl font-poppins text-pink-600 bg-white px-3 py-1 rounded-lg">ups🥲 Halaman yang kamu cari tidak ditemukan 💖</p>
+                <p className="font-extrabold text-xs md:text-2xl font-poppins text-pink-600 bg-white px-3 py-1 rounded-lg ">ups🥲 Halaman yang kamu cari tidak ditemukan 💖</p>
             </div>
         </div>
     );
