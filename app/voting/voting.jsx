e406e428ca997ecad6fd5576dd7a3e88b8546ea1
abdullah faqih muzakki <abdullahfaqihmuzakki@gmail.com>
2024-10-20 22:44:42 +0700

    display /voting minus animasi loading

--- app/voting/voting.jsx
+++ app/voting/voting.jsx
@@ -1,9 +1,37 @@
 "use client";
 
+import axios from 'axios'
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import { useNominasiDll } from "../context/nominasiDll";
 
 const Hero = () => {
-  const { selectedNominasi } = useNominasiDll()
+  const { selectedNominasi } = useNominasiDll();
+  const [details, setDetails] = useState({
+    "result": "",
+    "status": 100,
+    "shortDesc": "",
+    candidates: []
+  });
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await axios.post('api/getNominationDetails', {
+              "nominasi": `${selectedNominasi}`
+          })
+            if(response.status != 200) {
+              router.push('/nominasi')
+            }
+            setDetails(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            router.push('/nominasi')
+        }
+    }
+    fetchData();
+  }, [selectedNominasi]);
 
   return (
     <div className="relative h-auto bg-cover bg-center">
@@ -63,7 +91,7 @@ const Hero = () => {
               </div>
               <div className="rounded-lg bg-white py-3 mx-5 md:mx-14 lg:mx-80 text-center mt-4 md:mt-8">
                 <h3 className="text-base sm:text-lg md:text-2xl font-poppins font-semibold text-[#6F2A3D] transition duration-700">
-                  Penjelasan singkat mengenai kategori.
+                  {details.shortDesc}
                 </h3>
               </div>
             </div>
@@ -72,22 +100,22 @@ const Hero = () => {
           <div className="mt-8 sm:mt-14">
             {/* Flex grid for responsive layout */}
             <div className="flex flex-wrap justify-center gap-4 w-full transition duration-700">
-              {Array(7).fill(0).map((_, index) => (
-                <div key={index} className="relative w-[45%] sm:w-[45%] md:w-[39%] lg:w-[22%] h-[21rem] sm:h-[24rem] md:h-[32rem] bg-white rounded-lg shadow-lg overflow-hidden mb-4">
+              {details.candidates.map(candidate => (
+                <div key={candidate.nama} className="relative w-[45%] sm:w-[45%] md:w-[39%] lg:w-[22%] h-[21rem] sm:h-[24rem] md:h-[32rem] bg-white rounded-lg shadow-lg overflow-hidden mb-4">
                   <img
-                    src="/realityclub.svg"
+                    src={candidate.imgRoute}
                     alt="Nama Nominasi"
                     className="w-full h-full object-cover"
                   />
                   <div className="p-4 absolute bottom-0 w-full bg-gradient-to-t from-black from-50% to-transparent to-100%">
                     <h2 className="text-base sm:text-lg md:text-2xl lg:text-4xl font-poppins font-bold text-white">
-                      Nama Nominasi
+                      {candidate.nama}
                     </h2>
                     <p className="text-xs sm:text-xs md:text-sm lg:text-base font-poppins font-normal text-white mb-2">
                       (Jurusan - Angkatan)
                     </p>
                     <p className="text-xs sm:text-xs md:text-sm lg:text-base font-poppins font-normal text-white mt-2">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      {candidate.deskripsi}
                     </p>
                     <button className="mt-4 py-2 px-4 bg-pink-600 text-white font-semibold rounded-lg text-sm hover:bg-pink-700 w-full">
                       Vote
